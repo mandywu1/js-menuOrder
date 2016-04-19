ecc62f63603fed9667e06e303756cdf05f81ecbf
mandywu1 <mandywu@11.dhcp.wustl.edu>
2016-04-19 17:04:00 -0500

    1

--- menu.js
+++ menu.js
@@ -1,15 +1,32 @@
 // begin with a blank/new order
-var order = '';
+var order = 'Your order will include ';
 
 
 // build a function to ask questions
 
-function question() {
-  // your code goes here
+function question(item, choice) {
+  var answerInput = prompt("Do you want " + item + "?");
+
+if (answerInput.toUpperCase() === choice) {
+    order += item + ", ";
+}
+else {
+  order += "no " + item + ", ";
+}
+
 }
 
 // call the QUESTION function with the order options
-question();
+question("Meat", "YES");
+question("Rice", "YES");
+question("Beans", "YES");
+question("Lettuce", "YES");
+question("Cheese", "YES");
+question("Pico", "YES");
+question("Salsa", "YES");
+question("Guac", "YES");
+question("Corn", "YES");
+question("Sour cream", "YES");
 
 // output their order
-document.write("<p>You ordered: " + order + "<p>");
+document.write("<p> Order: " + order + "<p>");
